2c878c0e2e1921e984a8b70137e5d18bd312e3a9
vutran <vutran1412@icloud.com>
2019-06-11 20:15:49 -0700

    Initial Commit

---
+++ WhatsTheWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsTheWeather
+//
+//  Created by Vu Tran on 6/11/19.
+//  Copyright © 2019 Vu Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
